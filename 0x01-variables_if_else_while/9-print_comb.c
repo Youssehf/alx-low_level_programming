21209f42d6cafaec5e805b8174cf74e82cdc0df8
youssef <youssefsayed1352@gmail.com>
2023-07-15 22:35:31 +0000

    last few tasks

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: prints all combinations of single-digit number
+  *Returns: Always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
