c0a5e82fb370d9a435edb1269358872ea4cfa433
youssef <youssefsayed1352@gmail.com>
2023-07-17 19:03:49 +0000

    task 0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
   * Description: A C program that prints with put function
   * Return: Always 0 (Success)
   */
-int main(void
-){
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
