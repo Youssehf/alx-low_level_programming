dd726e393f98283fd032c57e20028deb55420a8b
youssef <youssefsayed1352@gmail.com>
2023-07-25 23:10:39 +0000

    project 5 in c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  *reset_to_98 - Takes a pointer to an integer and update it
+  *@n: the pointer to an integer
+  *
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is my 5th project in c
