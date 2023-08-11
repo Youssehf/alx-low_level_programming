4757793ca754d0caad5d274dfb76a625478ca1a7
youssef <youssefsayed1352@gmail.com>
2023-08-11 21:20:14 +0000

    Add changes to 0x0A-argc_argv/0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ *main - Print the name of the program
+ *@argc: Count arguments
+ *@argv: Arguments
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-(void) argc;
-printf("%s\n", argv[0]);
+	(void) argc;
 
-return (0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
