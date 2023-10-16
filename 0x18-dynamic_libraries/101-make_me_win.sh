dfd52cb39507627eb92b05648de1e17ce7feec6d
Youssehf <130256186+Youssehf@users.noreply.github.com>
2023-10-16 23:29:49 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/Cofucan/alx-low_level_programming/raw/main/0x18-dynamic_libraries/jackpot.so
+wget -P ../ https://github.com/Youssehf/alx-low_level_programming/raw/main/0x18-dynamic_libraries/jackpot.so
 export LD_PRELOAD=../jackpot.so
